d4d8d476159e636a390ac59bf449e082ec29c244
川田剣士 <kenji.k.4506@gmail.com>
2021-02-16 14:27:11 +0900

    change from English to Japanese in devise

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,26 @@ module Webapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # 以下を追加すれば日本語に
+    config.i18n.default_locale = :ja
+    # タイムゾーンも変更するなら，以下を追加
+    config.time_zone = 'Asia/Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        # falseはテストデータベースにレコードを作成するファイルの作成をスキップしている。
+        fixtures: false,
+        # falseはビュースペックを作成しないことを指定している。
+        view_specs: false,
+        # falseはヘルパーファイル用のスペックを作成しないことを指定している。
+        helper_specs: false,
+        # falseはconfig/routes.rb用のスペックファイルの作成を省略している。
+        routing_specs: false
+    end
   end
 end
